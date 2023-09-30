ffd0b4262cd4f9f21fa8071ef82ba84cc72516ec
rasanjana-baryon-at-619377243340 <rasadilshanganegoda1997@gmail.com>
2023-10-01 00:25:50 +0530

    Initial commit

---
+++ backend/base/api/views.py
@@ -0,0 +1,44 @@
+from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from .serializers import NoteSerializer
+from base.models import Note
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+
+        return token
+    
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+#this is the view that we include all the api in our application to single place
+@api_view(['GET'])
+def getRoutes(request):
+
+    routes = [
+        '/api/token', #this is for get the refresh and access token for the username and password
+        '/api/token/refresh', #this will give you new token based on the refresh token 
+        '/api/notes',
+    ]
+
+    return Response(routes)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getNotes(request):
+    user = request.user
+    notes = user.note_set.all()
+    serializer = NoteSerializer(notes, many=True) 
+    return Response(serializer.data)
